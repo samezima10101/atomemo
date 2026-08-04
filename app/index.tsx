2f29f40c5a646a6385435ce48825ef9f5257852a
misatoku <baskeaoi@icloud.com>
2026-08-05 02:53:48 +0900

    feat:画面を真っ白に変更

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View } from "react-native";
 
 export default function HomeScreen() {
   return <View style={styles.container} />;
@@ -7,6 +7,6 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#FFFFFF',
+    backgroundColor: "#FFFFFF",
   },
 });
