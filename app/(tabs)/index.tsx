59929dd852713eac7f1c937f49e00d2f9fb184ac
misatoku <baskeaoi@icloud.com>
2026-08-05 15:27:16 +0900

    feat:NativeWindの追加。

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,21 +1,9 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 
 export default function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>あとメモ</Text>
+    <View className="flex-1 bg-white p-6">
+      <Text className="text-4xl font-bold">あとメモ</Text>
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#FFFFFF",
-    padding: 24,
-  },
-  title: {
-    fontSize: 28,
-    fontWeight: "bold",
-  },
-});
---
+++ app/(tabs)/reflections.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from "react-native";
+
+export default function HomeScreen() {
+  return (
+    <View className="flex-1 bg-white p-6">
+      <Text className="text-2xl font-bold">あとメモ</Text>
+    </View>
+  );
+}
