b87fd18a5d0c3bbfa0877107475e8a09f08479f4
misatoku <baskeaoi@icloud.com>
2026-08-06 09:14:15 +0900

    feat:ディレクトリ構成

---
+++ src/types/task.ts
@@ -0,0 +1,9 @@
+type Task = {
+  id: string;
+  title: string;
+  description: string | null;
+  taskDate: string;
+  isComplleted: boolean;
+  reflection: string | null;
+  completedAt: string | null;
+};
