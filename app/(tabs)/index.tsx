7e21e463fc47b2e182cd304963040db5ee593379
tkhsmkt <tkhsmkt16@gmail.com>
2026-08-12 10:57:55 +0900

    feat/一週間分のカレンダーの表示

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,9 +1,34 @@
-import { Text, View } from "react-native";
+import DateItem from "@/src/components/calendar/DateItem";
+import { useState } from "react";
+import { View } from "react-native";
+
+const dateItems = [
+  { date: "1", content: "月" },
+  { date: "2", content: "火" },
+  { date: "3", content: "水" },
+  { date: "4", content: "木" },
+  { date: "5", content: "金" },
+  { date: "6", content: "土" },
+  { date: "7", content: "日" },
+];
 
 export default function HomeScreen() {
+  const [selectedDate, setSelectedDate] = useState("1");
+
   return (
     <View className="flex-1 bg-white p-6">
-      <Text className="text-4xl font-bold">あとメモ</Text>
+      <View className="flex-row justify-between items-center space-x-1">
+        {dateItems.map((item) => (
+          <DateItem
+            key={item.date}
+            date={item.date}
+            content={item.content}
+            selected={item.date === selectedDate}
+            highlighted={item.date === "2" && item.content === "火"}
+            onPress={() => setSelectedDate(item.date)}
+          />
+        ))}
+      </View>
     </View>
   );
 }
