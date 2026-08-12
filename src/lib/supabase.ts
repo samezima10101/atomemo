acd42176a51f52ea540b93aafddee80df042c493
sahono6363 <mumi6363n@gmail.com>
2026-08-12 10:17:46 +0900

    supabaseインストール修正

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,6 +1,5 @@
 import { createClient } from '@supabase/supabase-js';
 
-// 環境変数が読み込まれていない場合の型エラーを防ぐため ! をつける
 const supabaseID = process.env.NEXT_PUBLIC_SUPABASE_ID!;
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
